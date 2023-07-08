46e173e3526cf1b9cbd68b5f8fed775e1ecabfb5
Okezie Nibo <nibomicheal@gmail.com>
2023-07-08 16:54:39 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,67 @@
+//
+
+//console.log("Welcome to Javascript live class with Setemi Ojo")
+
+//console.log("Hello World")
+//console.log()
+// data types in javascript
+// primitive data types
+//let a = 10;
+//let a be a number
+let a;
+a = 10; // assigning a value(number) to a variable
+const ispresident = true;
+const pi = 22/7;
+let number = 22.7;
+let another_Number = -309 
+// consloe.log(typeof number);
+
+// typeof
+let mrUndefined;
+console.log(mrUndefined);
+// conversion
+//explicit conversion
+// Number() - converts to number. parseint(). parseFloat(
+    {let stringNumber ="10a";
+    const number99 = Number(stringNumber);
+    console.log(number99);
+
+}
+print(typeof number99); 
+
+console.log(typeof Nan)
+
+let number99String = String(number99);
+console.log(typeof numbeer99String); 
+console.log(true)
+console.log(boolean(NaN))
+// Bigint conversion
+// if ojo is a student than show his result
+// syntax of conditional statement 
+// if (condition) { // - condition of the expression must be tru or false}
+// code to be executed if condition is true
+let result = 99;
+let ojoStudentId = 2023354747;
+let ojoType = 'student';
+// function declaration
+
+for (let i = 0; i < 10; 1++) {
+    functionName()
+}
+function functionName () {
+ console.log('function')   
+}
+function add (a, b){
+    let c = a + b;
+    return c;
+}
+
+// local variable
+// outer variable
+// global variable
+console.log(globalThis)
+
+// default values in function
+// function expression: you are turning a function declaration in to a variable
+
+
